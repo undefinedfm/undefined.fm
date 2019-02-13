c433bbe0ce1c66341159152a50c0d9ea119bcc3e
Jared Palmer <jared@palmer.net>
2019-02-12 21:51:18 -0500

    Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,9 @@ import { Row, Block, Column } from 'glamor/jsxstyle';
 import { NavLink } from './Nav/NavLink';
 import { LINKS } from '../constants';
 import { Container } from './Container';
-
+import { Formik, Form, FieldProps } from 'formik';
+import axios from 'axios';
+import { Fieldset } from './Fieldset';
 export interface FooterProps {}
 
 export const Footer: React.SFC<FooterProps> = props => {
@@ -16,6 +18,130 @@ export const Footer: React.SFC<FooterProps> = props => {
       textAlign="center"
     >
       <Container>
+        <Formik
+          initialValues={{ name: '', email: '' }}
+          onSubmit={(values, { setSubmitting, setStatus }) => {
+            axios
+              .request({
+                url:
+                  'https://api.formik.com/v1/form/5bf8a927daa4d40001e38e2a/submit',
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/json',
+                },
+                data: {
+                  ...values,
+                },
+              })
+              .then(
+                () => {
+                  setSubmitting(false);
+                  setStatus('Success');
+                },
+                e => {
+                  console.log(e);
+                }
+              );
+          }}
+        >
+          {({ status, isSubmitting }) => (
+            <>
+              {status === 'Success' ? (
+                <Block
+                  component="h2"
+                  textAlign="center"
+                  marginBottom="2rem"
+                  fontWeight="800"
+                  fontStyle="italic"
+                  textTransform="uppercase"
+                  color="#C86DD7"
+                >
+                  Thanks! We'll keep you posted!
+                </Block>
+              ) : (
+                <Form>
+                  <Block
+                    component="h2"
+                    textAlign="center"
+                    marginBottom="2rem"
+                    fontWeight="800"
+                    fontStyle="italic"
+                    textTransform="uppercase"
+                    color="#C86DD7"
+                  >
+                    Subscribe for updates.
+                  </Block>
+                  <Fieldset
+                    id="name"
+                    name="name"
+                    label="Name"
+                    render={({ field }: FieldProps) => (
+                      <Block
+                        component="input"
+                        props={{
+                          ...field,
+                          placeholder: 'Name',
+                          required: true,
+                        }}
+                        css={inputStyles}
+                      />
+                    )}
+                  />
+                  <Fieldset
+                    label="Email"
+                    id="email"
+                    name="email"
+                    render={({ field, form }: FieldProps) => (
+                      <Block
+                        component="input"
+                        props={{
+                          ...field,
+                          type: 'email',
+                          placeholder: 'Email',
+                          required: true,
+                        }}
+                        css={inputStyles}
+                      />
+                    )}
+                  />
+                  <Block
+                    component="button"
+                    props={{ type: 'submit' }}
+                    color={theme.color.white}
+                    padding="1rem 1.25rem"
+                    minWidth={200}
+                    margin="3rem auto"
+                    display="block"
+                    position="relative"
+                    textTransform="uppercase"
+                    borderRadius={30}
+                    border="0"
+                    fontSize="1rem"
+                    textAlign="center"
+                    fontWeight={theme.bold}
+                    transform="translateY(0)"
+                    cursor="pointer"
+                    background="#3023AE"
+                    fontStyle="italic"
+                    backgroundImage="linear-gradient(-134deg, #3023AE 0%, #C86DD7 100%)"
+                    css={{
+                      transition: 'all 100ms ease',
+                      '&:hover': {
+                        transform: `translateY(-1px)`,
+                        boxShadow: `0 4px 8px rgba(0,0,0,.1)`,
+                      },
+                      '&:active': {
+                        transform: `translateY(0)`,
+                      },
+                    }}
+                  >
+                    {isSubmitting ? 'Loading....' : 'Subscribe'}
+                  </Block>
+                </Form>
+              )}
+            </>
+          )}
+        </Formik>
         <Column
           margin="0 auto 2rem"
           css={{
