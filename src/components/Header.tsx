1816b882a3ff45952f31753dbd65591a635578c5
Erik Rasmussen <rasmussenerik@gmail.com>
2019-03-06 21:11:24 +0100

    Added space between "Hosted by" and "Jared"
    One of the most annoying things about JSX. 🙄

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -22,7 +22,7 @@ export function Header(props: any) {
           opacity: 0.9,
         })}
       >
-        The only engineering podcast with a 2 drink minimum. Hosted by
+        The only engineering podcast with a 2 drink minimum. Hosted by{' '}
         <a
           {...css({ textDecoration: 'none', fontWeight: 700 })}
           href="http://twitter.com/jaredpalmer"
